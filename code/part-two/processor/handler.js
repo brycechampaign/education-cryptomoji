df35f6603923dd127b972e5f251e39dcfd43fffc
Bryce Champaign <bechampaign@protonmail.com>
2019-12-27 13:49:13 -0500

    Implement handling for createCollection actions

--- code/part-two/processor/handler.js
+++ code/part-two/processor/handler.js
@@ -2,8 +2,13 @@
 
 const { TransactionHandler } = require('sawtooth-sdk/processor/handler');
 const { InvalidTransaction } = require('sawtooth-sdk/processor/exceptions');
-const { decode } = require('./services/encoding');
-
+const { decode, encode } = require('./services/encoding');
+const {
+  getCollectionAddress,
+  getMojiAddress
+} = require('./services/addressing');
+const getPrng = require('./services/prng');
+const { createHash } = require('crypto');
 
 const FAMILY_NAME = 'cryptomoji';
 const FAMILY_VERSION = '0.1';
@@ -18,9 +23,9 @@ class MojiHandler extends TransactionHandler {
    * validator, declaring which family name, versions, and namespaces it
    * expects to handle. We'll fill this one in for you.
    */
-  constructor () {
+  constructor() {
     console.log('Initializing cryptomoji handler with namespace:', NAMESPACE);
-    super(FAMILY_NAME, [ FAMILY_VERSION ], [ NAMESPACE ]);
+    super(FAMILY_NAME, [FAMILY_VERSION], [NAMESPACE]);
   }
 
   /**
@@ -46,10 +51,69 @@ class MojiHandler extends TransactionHandler {
    *   - context.deleteState(addresses): deletes the state for the passed
    *     array of state addresses. Only needed if attempting the extra credit.
    */
-  apply (txn, context) {
-    // Enter your solution here
-    // (start by decoding your payload and checking which action it has)
+  apply(txn, context) {
+    const { payload, header, signature } = txn;
+    const publicKey = header.signerPublicKey;
+    let action;
 
+    return new Promise((resolve, reject) => {
+      try {
+        action = decode(payload).action;
+      } catch (err) {
+        reject(new InvalidTransaction('Poorly encoded payload'));
+      }
+
+      if (action === 'CREATE_COLLECTION') {
+        const address = getCollectionAddress(publicKey);
+        return context
+          .getState([address])
+          .then(state => {
+            if (JSON.stringify(state[address]) !== '[]') {
+              throw new InvalidTransaction('Signer already has a collection');
+            }
+
+            const moji = [];
+            const mojiAddresses = [];
+
+            for (let i = 0; i < 3; i++) {
+              const dna = createHash('sha512')
+                .update(JSON.stringify(getPrng(signature)(1000)))
+                .digest('hex')
+                .slice(0, 36);
+
+              const address = getMojiAddress(publicKey, dna);
+              mojiAddresses.push(address);
+
+              moji.push(
+                context.setState({
+                  [address]: encode({
+                    dna,
+                    owner: publicKey,
+                    breeder: null,
+                    sire: null,
+                    bred: null,
+                    sired: null
+                  })
+                })
+              );
+            }
+
+            return Promise.all(moji)
+              .then(() =>
+                context.setState({
+                  [address]: encode({
+                    key: publicKey,
+                    moji: mojiAddresses
+                  })
+                })
+              )
+              .then(addresses => resolve(addresses));
+          })
+          .catch(err => reject(err));
+      } else {
+        reject(new InvalidTransaction('Invalid action'));
+      }
+    });
   }
 }
 
